c6768b8cc46ece5ef597a767e601015b1a7e264e
gabel10 <130350@unsaac.edu.pe>
2017-02-05 19:18:21 -0500

    Cambios en consultorios

--- resources/views/consultorios/create.blade.php
+++ resources/views/consultorios/create.blade.php
@@ -1,52 +1,61 @@
 @extends('Consultorios.MantenimientoConsultorios')
 
 @section('estilos')
-
 @endsection
 
-@section('Titulo')
-    <i class="fa fa-gear fa-fw"></i>
-    <a1>Agregar Consultorio<a1>
-@endsection
-
-li>
 @section('Contenido')
+<form role="form" method="post" action="/consultorios">
+  <input type="hidden" name="_token" value="{{csrf_token()}}">
+  <fieldset>
+    <div class="text-center"><h2 align="center">NUEVO CONSULTORIO</h2>
+    </div></br>
 
-    <form role="form" method="post" action="/consultorios">
-      <input type="hidden" name="_token" value="{{csrf_token()}}">
-            <fieldset>
-            <div class="text-center"><h2 align="center">Agregar Consultorio</h2>
-            </div></br>
+    <div class="input-group col-md-12 col-xs-12">
+      <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-plus-square bigicon"></i></span>
+      <span align="center" class="col-md-6 col-xs-10 has-float-label">
+      <div align="center" class="group-control">
+        <input id="Dni" name="id" pattern = "[0-9]{3}"placeholder="Código del Consultorio" class="form-control" required>
+        <label>Código del Consultorio</label>
+      </div>
+    </div>
 
-            <div class="input-group col-md-12 col-xs-12">
-                <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-plus-square bigicon"></i></span>
-                <span align="center" class="col-md-6 col-xs-10 has-float-label">
-                  <div align="center" class="group-control">
-                    <input id="Dni" name="id" placeholder="Código del Consultorio" class="form-control" required>
-                    <label>Código del Consultorio</label>
-                  </div>
-            </div>
-            <div class="input-group col-md-12 col-xs-12">
-                <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-h-square bigicon"></i></span>
-                <span align="center" class="col-md-6 col-xs-10 has-float-label">
-                  <div align="center" class="group-control">
-                    <input id="Dni" name="ubicacion" placeholder="Nombre del Consultorio" class="form-control" required>
-                    <label>Nombre del Consultorio</label>
-                  </div>
-            </div></br>
+    <div class="input-group col-md-12 col-xs-12">
+      <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-h-square bigicon"></i></span>
+      <span align="center" class="col-md-6 col-xs-10 has-float-label">
+      <div align="center" class="group-control">
+        <input id="Dni" name="ubicacion" placeholder="Nombre del Consultorio" class="form-control" required>
+        <label>Nombre del Consultorio</label>
+      </div>
+    </div></br>
 
-            <div class="form-group">
-                  <div class="col-md-12 text-center">
-                      <input type="submit" style="width:80px" class="btn btn-success" align="center" class="form-control" value="Agregar">
-                      <button type="reset" style="width:80px" class="btn btn-primary" align="center" class="form-control" >Limpiar</button>
+    <div class="form-group">
+      <div class="col-md-12 text-center">
+        <a data-target="#confirmar" data-toggle="modal" style="width:80px" class="btn btn-success" align="center" class="form-control" value="Guardar">Guardar</a>
+        <button type="reset" style="width:80px" class="btn btn-primary" align="center" class="form-control" >Limpiar</button>
+      </div>
+    </div></br></br>
 
-                  </div>
-            </div></br></br>
-
-            </fieldset>
-
-       </form>
-
+    <div class="modal fade modal-slide-in-rigth" aria-hidden="true" role="dialog" tabindex="-1" id="confirmar">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-Label="Close">
+              <span aria-hidden="true">X</span>
+            </button>
+            <h3 class="modal-title">Agregar Consultorio</h3>
+          </div>
+          <div class="modal-body">
+            <p>¡Desea agregar este consultorio?</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            <button type="submit" style="width:80px" class="btn btn-success">Guardar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+</form>
 @endsection
 
 @section('js')
--- resources/views/consultorios/edit.blade.php
+++ resources/views/consultorios/edit.blade.php
@@ -1,53 +1,60 @@
 @extends('consultorios.MantenimientoConsultorios')
 
-@section('Titulo')
-<i class="fa fa-gear fa-fw"></i>
-<a1>Mantenimiento de consultorios<a1>
-
-@endsection
-
-
 @section('Contenido')
+<form method="POST" action="/consultorios/{{ $consultorios->id }}" autocomplete="off">
+{{csrf_field()}}
+{{method_field('PUT')}}
+  <fieldset>
+    <div class="text-center"><h2 align="center">MODIFICAR CONSULTORIO</h2>
+    </div></br>
 
+    <div class="input-group col-md-12 col-xs-12">
+      <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-plus-square bigicon"></i></span>
+      <span align="center" class="col-md-6 col-xs-10 has-float-label">
+      <div align="center" class="group-control">
+        <input id="codigo" name="id" placeholder="Código del Consultorio" class="form-control" value="{{$consultorios->id}}" required disabled="true">
+        <label>Código del Consultorio</label>
+      </div>
+    </div>
 
-        <form method="POST" action="/consultorios/{{ $consultorios->id }}" autocomplete="off">
+    <div class="input-group col-md-12 col-xs-12">
+      <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-h-square bigicon"></i></span>
+      <span align="center" class="col-md-6 col-xs-10 has-float-label">
+      <div align="center" class="group-control">
+        <input id="nombre" name="ubicacion" placeholder="Ubicación del Consultorio" class="form-control" value="{{$consultorios->ubicacion}}" required>
+        <label>Nombre del Consultorio</label>
+      </div>
+    </div></br>
 
-          {{csrf_field()}}
-          {{method_field('PUT')}}
+    <div class="form-group">
+      <div class="col-md-12 text-center">
+        <a data-target="#confirmar-{{ $consultorios->id }}" data-toggle="modal" style="width:80px"
+          class="btn btn-success" align="center" class="form-control" value="Guardar" >Guardar</a>
+        <button type="reset" style="width:80px" class="btn btn-primary" align="center" class="form-control" >Limpiar</button>
+      </div>
+    </div></br></br>
 
-            <fieldset>
-
-            <div class="text-center"><h2 align="center">Modificar consultorio</h2>
-            </div></br>
-
-            <div class="input-group col-md-12 col-xs-12">
-                <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-plus-square bigicon"></i></span>
-                <span align="center" class="col-md-6 col-xs-10 has-float-label">
-                  <div align="center" class="group-control">
-                    <input id="Dni" name="id" placeholder="Código del Consultorio" class="form-control" value="{{$consultorios->id}}" required disabled="true">
-                    <label>Código del Consultorio</label>
-                  </div>
-            </div>
-            <div class="input-group col-md-12 col-xs-12">
-                <span class="col-md-1 col-xs-1 col-md-offset-2 text-center"><i class="fa fa-h-square bigicon"></i></span>
-                <span align="center" class="col-md-6 col-xs-10 has-float-label">
-                  <div align="center" class="group-control">
-                    <input id="Password" name="ubicacion" placeholder="Ubicación del Consultorio" class="form-control" value="{{$consultorios->ubicacion}}" required>
-                    <label>Nombre del Consultorio</label>
-                  </div>
-            </div></br>
-            
-                <div class="form-group">
-                  <div class="col-md-12 text-center">
-                      <input type="submit" style="width:80px" class="btn btn-success" align="center" class="form-control" value="Guardar">
-                      <button type="reset" style="width:80px" class="btn btn-primary" align="center" class="form-control" >Limpiar</button>
-
-                  </div>
-            </div></br></br>
-
-
-            </fieldset>
-
-
-       </form>
+    <div class="modal fade modal-slide-in-rigth" aria-hidden="true" role="dialog" tabindex="-1" id="confirmar-{{$consultorios->id}}">
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-Label="Close">
+              <span aria-hidden="true">X</span>
+            </button>
+            <h3 class="modal-title">Modificar Consultorio</h3>
+          </div>
+          <div class="modal-body">
+            <p>¿Desea guardar los cambios efectuados en este consultorio?</p>
+            <p>Codigo: {{$consultorios->id}}</p>
+            <p>Nombre: {{$consultorios->ubicacion}}</p>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Cerrar</button>
+            <button type="submit" style="width:80px" class="btn btn-success">Guardar</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </fieldset>
+</form>
 @endsection
